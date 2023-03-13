17acd6aa9018947911c987b6fb880e5db2bd5415
Henry <xuanhoa.henry@gmail.com>
2023-03-13 19:28:06 +0700

    Fix requestDefault => requestDefaults

--- api/unsendMessage.js
+++ api/unsendMessage.js
@@ -1,9 +1,9 @@
-module.exports = function({ requestDefault, utils }) {
+module.exports = function({ requestDefaults, utils }) {
     var { makeCallback } = utils;
     return async function(messageID, callback) {
         if (!callback) callback = makeCallback();
         if (!messageID || !String.isString(messageID)) return callback('Please pass a messageID in the first arguments', null);
-        var response = await requestDefault.post('https://www.facebook.com/messaging/unsend_message/', { message_id: messageID });
+        var response = await requestDefaults.post('https://www.facebook.com/messaging/unsend_message/', { message_id: messageID });
         if (response.error) return callback(response.error, false);
         else callback(null, true);
     }
