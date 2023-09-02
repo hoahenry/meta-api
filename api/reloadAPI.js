b3bc222887fd377b4fdd9a03d3ed785a8023b9b2
Chử Xuân Hòa <xuanhoa.henry@gmail.com>
2023-09-02 19:42:37 +0700

    small fix

--- api/reloadAPI.js
+++ api/reloadAPI.js
@@ -3,6 +3,7 @@ var { readdirSync } = require('fs');
 module.exports = function({ browser, request, client, log, api, utils }) {
     var apiPath = __dirname + '/';
     return async function(callback) {
+        if (!callback || !Function.isFunction(callback)) callback = utils.makeCallback();
         var apiName = readdirSync(apiPath).map(name => name.replace(/\.js/, ''));
         for (let name of apiName) {
             if (require.cache[require.resolve(apiPath + name)]) delete require.cache[require.resolve(apiPath + name)];
