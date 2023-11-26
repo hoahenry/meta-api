84c9ff3046fb924d4ce465e0ad412345da6a41e7
Chử Xuân Hòa <xuanhoa.henry@gmail.com>
2023-11-26 15:12:32 +0700

    Minor changes to code structure and whitespace handling in input.

--- api/getUserID.js
+++ api/getUserID.js
@@ -1,22 +1,39 @@
 module.exports = function({ request, utils, Language }) {
     var isURL = /^https?:\/\/.*$/, hasUserData = /"props":({.*?})/g;
-    return async function(input, callback) {
-        var originalURL = 'https://www.facebook.com';
-        if (!callback || !Function.isFunction(callback)) callback = utils.makeCallback();
-        if (!input) return callback(Language('getUserID', 'needURL'));
-        if (!isURL.test(input)) originalURL += '/' + input;
-        else {
-            var _URL = new URL(input);
-            originalURL += _URL.pathname.includes('/profile.php') ? `/${_URL.searchParams.get('id')}` : _URL.pathname;
+    
+    function buildURL(input) {
+        const baseURL = 'https://www.facebook.com/';
+        const trimmedInput = input.trim();
+        if (!isURL.test(trimmedInput)) return baseURL + trimmedInput;
+    
+        const urlObject = new URL(trimmedInput);
+        const path = urlObject.pathname.includes('/profile.php') ? urlObject.searchParams.get('id') : urlObject.pathname;
+        return baseURL + path;
+    }
+    
+    function extractUserData(body) {
+        const match = hasUserData.exec(body);
+        if (!match || !match[1]) throw new Error(Language('getUserID', 'error', originalURL));
+    
+        const userDataJson = JSON.parse(match[1]);
+        return {
+            userVanity: userDataJson.userVanity || undefined,
+            userID: userDataJson.userID,
+            eligibleForProfilePlusEntityMenu: userDataJson.eligibleForProfilePlusEntityMenu
+        };
+    }
+
+    return async function getUserID(input, callback) {
+        try {
+            if (!callback || !Function.isFunction(callback)) callback = utils.makeCallback();
+            if (!input.trim()) return callback(Language('getUserID', 'needURL'));
+    
+            const originalURL = buildURL(input);
+            const { body } = await request.get(originalURL);
+            const userData = extractUserData(body);
+            return callback(null, userData);
+        } catch (error) {
+            return callback(error.message);
         }
-        var { body } = await request.get(originalURL);
-        var _m = hasUserData.exec(body);
-        if (!_m || !_m[1]) return callback(Language('getUserID', 'error', originalURL));
-        var s2j = JSON.parse(_m[1]);
-        return callback(null, {
-            userVanity: s2j.userVanity || undefined,
-            userID: s2j.userID,
-            eligibleForProfilePlusEntityMenu: s2j.eligibleForProfilePlusEntityMenu
-        });
     }
 }
