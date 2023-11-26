4c27fc6ec8b47c6a5b29260bec7dc7fdcc570795
Chử Xuân Hòa <xuanhoa.henry@gmail.com>
2023-11-26 15:15:21 +0700

    Small fixes
    small fix, return client.configs.

--- api/setConfigs.js
+++ api/setConfigs.js
@@ -1,9 +1,9 @@
 module.exports = function({ client, utils, Language }) {
     return function(options, callback) {
         if (!callback || !Function.isFunction(callback)) callback = utils.makeCallback();
-        var allowedProperties = Object.keys(client.configs), clientConfigProperties = Object.keys(configs);
+        var allowedProperties = Object.keys(client.configs), clientConfigProperties = Object.keys(options);
         if (clientConfigProperties.some(item => !allowedProperties.includes(item))) return callback(Language('system', 'unrecognizedConfigs', clientConfigProperties.filter(item => !allowedProperties.includes(item)).join(', ')));
         clientConfigProperties.filter(item => allowedProperties.includes(item)).forEach(item => client.configs[item] = options[item]);
-        return callback(null);
+        return callback(null, client.configs);
     }
 }
